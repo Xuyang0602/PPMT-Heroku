c4e560e4fb1f260cce0acabdbfbf2e0e7ce86432
xuyangwang0827 <xuyangwang0827@gmail.com>
2019-05-14 17:57:21 -0700

    Client-side React

---
+++ src/main/resources/static/precache-manifest.1a39753bcc029c4cd3fdd4180cd09daa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3299670f7b420a3704cb",
+    "url": "/static/css/main.f19fa6c8.chunk.css"
+  },
+  {
+    "revision": "3299670f7b420a3704cb",
+    "url": "/static/js/main.a07cd53c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5880168f9e7a2a89d0dc",
+    "url": "/static/css/2.61181b6a.chunk.css"
+  },
+  {
+    "revision": "5880168f9e7a2a89d0dc",
+    "url": "/static/js/2.f8baf648.chunk.js"
+  },
+  {
+    "revision": "3d654e06572268373f7616dabfae4c67",
+    "url": "/index.html"
+  }
+];
